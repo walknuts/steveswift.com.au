c0cbe74cd75c30247341d83fa9c89e3f440f67fe
Ben Swift <ben@benswift.me>
2022-01-23 14:42:18 +1100

    close to ready to do the actual import

--- migration/migrate_posts.py
+++ migration/migrate_posts.py
@@ -1,14 +1,43 @@
 import feedparser
 from markdownify import markdownify
+from datetime import datetime
+from pathlib import Path
 
 ## handy for local dev
 from icecream import ic
 
 d = feedparser.parse("https://steveswift.com.au/feed/")
 
+def jekyll_post_date(args):
+    ## TODO the datetime is actually returned as a 'published_parsed' field, but it's not a datetime, so...
+    return datetime.strptime(post["published"][0:16], "%a, %d %b %Y")
+
+def jekyll_post_name(post):
+    title = post["title"]
+    date = jekyll_post_date(post)
+    ## a bit gross, but it'll do for now
+    return f"{datetime.strftime(date, f'%Y-%m-%d')}-{title.replace(' ', '-')}.md".lower().replace(":", "").replace(",", "")
+
+def post_body_as_md(post):
+    if len(post["content"]) > 1:
+        ic(post)
+        raise ValueError(f"Multiple content fields for {post['title']}")
+
+    markdownify(post["content"][0]["value"])
+
 for post in d["entries"]:
+
+    post_path = Path("../_posts")
+    filename = jekyll_post_name(post)
     title = post["title"]
     tags = post["tags"]
-    date = post["published"]
-    body = markdownify(post["content"][0]["value"])
-    ic(title, body)
+    date = jekyll_post_date(post)
+    body = post_body_as_md(post)
+
+    with open(post_path / filename, mode='w') as f:
+        f.write_text(f"""---
+title: "{title}"
+tags: {tags}
+---
+
+{body}""")
